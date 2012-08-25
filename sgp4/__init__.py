ab44c06373d6a140f6e84f97ba863be8558cea15
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-24 22:05:34 -0400

    Added more information to the PyPI entry

--- sgp4/__init__.py
+++ sgp4/__init__.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-"""Computes earth satellite positions using up-to-date 2006 algorithm
+"""Track earth satellite TLE orbits using up-to-date 2010 version of SGP4
 
 This package can compute the position of an earth-orbiting satellite
-from the TLE orbital elements that can be downloaded from `Celestrak
+from the TLE orbital elements that you can download from `Celestrak
 <http://celestrak.com/>`_.  This implementation passes every one of the
-automated tests crafted by Vallado et al for their 2006 paper updating
-the traditional SGP4 algorithm:
+automated tests in the August 2010 release of SGP4 by Vallado et al,
+who originally published their revision of the algorithm in 2006:
 
     Vallado, David A., Paul Crawford, Richard Hujsak, and T.S. Kelso, “Revisiting Spacetrack Report #3,” presented at the AIAA/AAS Astrodynamics Specialist Conference, Keystone, CO, 2006 August 21–24.
 
 If you would like to review the paper, it is `available online
-<http://www.celestrak.com/publications/AIAA/2006-6753/>`_.
+<http://www.celestrak.com/publications/AIAA/2006-6753/>`_.  You can
+always download the latest version of their code for comparison against
+this Python module (or other implementations) as `AIAA-2006-6753.zip
+<http://www.celestrak.com/publications/AIAA/2006-6753/AIAA-2006-6753.zip>`_.
 
 """
