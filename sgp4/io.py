aedc0cfb38321e95e61f4a904d06b3809c04065d
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-25 16:19:38 -0400

    Converted everything but scanf()s to Python

--- sgp4/io.py
+++ sgp4/io.py
@@ -1,3 +1,12 @@
+"""Read the TLE earth satellite file format.
+
+This is a minimally-edited copy of "sgp4io.cpp".
+
+"""
+from math import pi
+from sgp4.propagation import getgravconst, sgp4init
+
+"""
 /*     ----------------------------------------------------------------
 *
 *                               sgp4io.cpp
@@ -26,9 +35,9 @@
 *              14 aug 06  david vallado
 *                           original baseline
 *       ----------------------------------------------------------------      */
+"""
 
-#include "sgp4io.h"
-
+"""
 /* -----------------------------------------------------------------------------
 *
 *                           function twoline2rv
@@ -68,63 +77,59 @@
 *    norad spacetrack report #3
 *    vallado, crawford, hujsak, kelso  2006
   --------------------------------------------------------------------------- */
+"""
 
-void twoline2rv
-     (
-      char      longstr1[130], char longstr2[130],
-      char      typerun,  char typeinput, char opsmode,
-      gravconsttype       whichconst,
-      double& startmfe, double& stopmfe, double& deltamin,
-      elsetrec& satrec
-     )
-     {
-       const double deg2rad  =   pi / 180.0;         //   0.0174532925199433
-       const double xpdotp   =  1440.0 / (2.0 *pi);  // 229.1831180523293
+def twoline2rv(
+      longstr1, longstr2,
+      typerun,  typeinput, opsmode,
+      whichconst,
+      satrec,
+      ):
 
-       double sec, mu, radiusearthkm, tumin, xke, j2, j3, j4, j3oj2;
-       double startsec, stopsec, startdayofyr, stopdayofyr, jdstart, jdstop;
-       int startyear, stopyear, startmon, stopmon, startday, stopday,
-           starthr, stophr, startmin, stopmin;
-       int cardnumb, numb, j;
-       long revnum = 0, elnum = 0;
-       char classification, intldesg[11];
-       int year = 0;
-       int mon, day, hr, minute, nexp, ibexp;
+       deg2rad  =   pi / 180.0;         #    0.0174532925199433
+       xpdotp   =  1440.0 / (2.0 *pi);  #  229.1831180523293
 
-       getgravconst( whichconst, tumin, mu, radiusearthkm, xke, j2, j3, j4, j3oj2 );
+       revnum = 0;
+       elnum = 0;
+
+       year = 0;
+       # int mon, day, hr, minute, nexp, ibexp;
+
+       tumin, mu, radiusearthkm, xke, j2, j3, j4, j3oj2 = getgravconst(whichconst)
 
        satrec.error = 0;
 
-       // set the implied decimal points since doing a formated read
-       // fixes for bad input data values (missing, ...)
-       for (j = 10; j <= 15; j++)
-           if (longstr1[j] == ' ')
+       #  set the implied decimal points since doing a formated read
+       #  fixes for bad input data values (missing, ...)
+       for j in xrange(10, 16):
+           if longstr1[j] == ' ':
                longstr1[j] = '_';
 
-       if (longstr1[44] != ' ')
+       if longstr1[44] != ' ':
            longstr1[43] = longstr1[44];
        longstr1[44] = '.';
-       if (longstr1[7] == ' ')
+       if longstr1[7] == ' ':
            longstr1[7] = 'U';
-       if (longstr1[9] == ' ')
+       if longstr1[9] == ' ':
            longstr1[9] = '.';
-       for (j = 45; j <= 49; j++)
-           if (longstr1[j] == ' ')
+       for j in xrange(45, 50):
+           if longstr1[j] == ' ':
                longstr1[j] = '0';
-       if (longstr1[51] == ' ')
+       if longstr1[51] == ' ':
            longstr1[51] = '0';
-       if (longstr1[53] != ' ')
+       if longstr1[53] != ' ':
            longstr1[52] = longstr1[53];
        longstr1[53] = '.';
        longstr2[25] = '.';
-       for (j = 26; j <= 32; j++)
-           if (longstr2[j] == ' ')
+       for j in xrange(26, 33):
+           if longstr2[j] == ' ':
                longstr2[j] = '0';
-       if (longstr1[62] == ' ')
+       if longstr1[62] == ' ':
            longstr1[62] = '0';
-       if (longstr1[68] == ' ')
+       if longstr1[68] == ' ':
            longstr1[68] = '0';
 
+       """
        sscanf(longstr1,"%2d %5ld %1c %10s %2d %12lf %11lf %7lf %2d %7lf %2d %2d %6ld ",
                        &cardnumb,&satrec.satnum,&classification, intldesg, &satrec.epochyr,
                        &satrec.epochdays,&satrec.ndot, &satrec.nddot, &nexp, &satrec.bstar,
@@ -156,18 +161,18 @@ void twoline2rv
                        &satrec.nodeo,&satrec.ecco, &satrec.argpo, &satrec.mo, &satrec.no,
                        &revnum );
          }
-
-       // ---- find no, ndot, nddot ----
-       satrec.no   = satrec.no / xpdotp; //* rad/min
+       """
+       #  ---- find no, ndot, nddot ----
+       satrec.no   = satrec.no / xpdotp; #   rad/min
        satrec.nddot= satrec.nddot * pow(10.0, nexp);
        satrec.bstar= satrec.bstar * pow(10.0, ibexp);
 
-       // ---- convert to sgp4 units ----
+       #  ---- convert to sgp4 units ----
        satrec.a    = pow( satrec.no*tumin , (-2.0/3.0) );
-       satrec.ndot = satrec.ndot  / (xpdotp*1440.0);  //* ? * minperday
+       satrec.ndot = satrec.ndot  / (xpdotp*1440.0);  #   ? * minperday
        satrec.nddot= satrec.nddot / (xpdotp*1440.0*1440);
 
-       // ---- find standard orbital elements ----
+       #  ---- find standard orbital elements ----
        satrec.inclo = satrec.inclo  * deg2rad;
        satrec.nodeo = satrec.nodeo  * deg2rad;
        satrec.argpo = satrec.argpo  * deg2rad;
@@ -176,6 +181,7 @@ void twoline2rv
        satrec.alta = satrec.a*(1.0 + satrec.ecco) - 1.0;
        satrec.altp = satrec.a*(1.0 - satrec.ecco) - 1.0;
 
+       """
        // ----------------------------------------------------------------
        // find sgp4epoch time of element set
        // remember that sgp4 uses units of days from 0 jan 1950 (sgp4epoch)
@@ -184,28 +190,33 @@ void twoline2rv
 
        // ---------------- temp fix for years from 1957-2056 -------------------
        // --------- correct fix will occur when year is 4-digit in tle ---------
-       if (satrec.epochyr < 57)
+       """
+       if satrec.epochyr < 57:
            year= satrec.epochyr + 2000;
-         else
+       else:
            year= satrec.epochyr + 1900;
 
        days2mdhms ( year,satrec.epochdays, mon,day,hr,minute,sec );
        jday( year,mon,day,hr,minute,sec, satrec.jdsatepoch );
 
-       // ---- input start stop times manually
-       if ((typerun != 'v') && (typerun != 'c'))
-         {
-         // ------------- enter start/stop ymd hms values --------------------
-           if (typeinput == 'e')
-             {
+       #  ---- input start stop times manually
+       if typerun != 'v' and typerun != 'c':
+
+         #  ------------- enter start/stop ymd hms values --------------------
+           if typeinput == 'e':
+
                printf("input start prop year mon day hr min sec \n");
-               // make sure there is no space at the end of the format specifiers in scanf!
+               #  make sure there is no space at the end of the format specifiers in scanf!
+               """
                scanf( "%i %i %i %i %i %lf",&startyear, &startmon, &startday, &starthr, &startmin, &startsec);
+               """
                fflush(stdin);
                jday( startyear,startmon,startday,starthr,startmin,startsec, jdstart );
 
                printf("input stop prop year mon day hr min sec \n");
+               """
                scanf( "%i %i %i %i %i %lf",&stopyear, &stopmon, &stopday, &stophr, &stopmin, &stopsec);
+               """
                fflush(stdin);
                jday( stopyear,stopmon,stopday,stophr,stopmin,stopsec, jdstop );
 
@@ -213,16 +224,19 @@ void twoline2rv
                stopmfe  = (jdstop - satrec.jdsatepoch) * 1440.0;
 
                printf("input time step in minutes \n");
+               """
                scanf( "%lf",&deltamin );
-             }
-           // -------- enter start/stop year and days of year values -----------
-           if (typeinput == 'd')
-             {
+               """
+
+           #  -------- enter start/stop year and days of year values -----------
+           if typeinput == 'd':
+
+               """
                printf("input start year dayofyr \n");
                scanf( "%i %lf",&startyear, &startdayofyr );
                printf("input stop year dayofyr \n");
                scanf( "%i %lf",&stopyear, &stopdayofyr );
-
+               """
                days2mdhms ( startyear,startdayofyr, mon,day,hr,minute,sec );
                jday( startyear,mon,day,hr,minute,sec, jdstart );
                days2mdhms ( stopyear,stopdayofyr, mon,day,hr,minute,sec );
@@ -232,32 +246,32 @@ void twoline2rv
                stopmfe  = (jdstop - satrec.jdsatepoch) * 1440.0;
 
                printf("input time step in minutes \n");
+               """
                scanf( "%lf",&deltamin );
-             }
-           // ------------------ enter start/stop mfe values -------------------
-           if (typeinput == 'm')
-             {
+               """
+
+           #  ------------------ enter start/stop mfe values -------------------
+           if typeinput == 'm':
+
+               """
                printf("input start min from epoch \n");
                scanf( "%lf",&startmfe );
                printf("input stop min from epoch \n");
                scanf( "%lf",&stopmfe );
                printf("input time step in minutes \n");
                scanf( "%lf",&deltamin );
-             }
-         }
+               """
 
-       // ------------ perform complete catalog evaluation, -+ 1 day ----------- 
-       if (typerun == 'c')
-         {
+       #  ------------ perform complete catalog evaluation, -+ 1 day -----------
+       if typerun == 'c':
+
            startmfe = -1440.0;
            stopmfe  =  1440.0;
            deltamin =    10.0;
-         }
 
-       // ---------------- initialize the orbit at sgp4epoch -------------------
+       #  ---------------- initialize the orbit at sgp4epoch -------------------
        sgp4init( whichconst, opsmode, satrec.satnum, satrec.jdsatepoch-2433281.5, satrec.bstar,
                  satrec.ecco, satrec.argpo, satrec.inclo, satrec.mo, satrec.no,
                  satrec.nodeo, satrec);
-    } // end twoline2rv
 
-
+       return startmfe, stopmfe, deltamin
