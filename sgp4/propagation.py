f3a301dbf795a3dd7e686b39ba71858d907de873
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-25 14:25:47 -0400

    Added intro docstring; hid C++ in Python string

--- sgp4/propagation.py
+++ sgp4/propagation.py
@@ -1,3 +1,23 @@
+"""The sgp4 procedures for analytical propagation of a satellite.
+
+I have made the rather unorthodox decision to leave as much of this C++
+code alone as possible: if a line of code would run without change in
+Python, then I refused to re-indent it or remove its terminal semicolon,
+so that in the future it will be easier to keep updating this file as
+the original author's C++ continues to improve.  Thus, 5-space
+indentation (!) prevails in this file.
+
+I have even kept all of the C++ block comments (by turning them into
+Python string constants) to make this easier to navigate and maintain,
+as well as to make it more informative for people who encounter this
+code for the first time here in its Python form.
+
+| - Brandon Rhodes
+|   Common Grounds Coffee House, Bluffton, Ohio
+|   On a very hot August day in 2012
+"""
+
+"""
 /*     ----------------------------------------------------------------
 *
 *                               sgp4unit.cpp
@@ -48,95 +68,9 @@
 *                     80  norad
 *                           original baseline
 *       ----------------------------------------------------------------      */
+"""
 
-#include "sgp4unit.h"
-
-const char help = 'n';
-FILE *dbgfile;
-
-
-/* ----------- local functions - only ever used internally by sgp4 ---------- */
-static void dpper
-     (
-       double e3,     double ee2,    double peo,     double pgho,   double pho,
-       double pinco,  double plo,    double se2,     double se3,    double sgh2,
-       double sgh3,   double sgh4,   double sh2,     double sh3,    double si2,
-       double si3,    double sl2,    double sl3,     double sl4,    double t,
-       double xgh2,   double xgh3,   double xgh4,    double xh2,    double xh3,
-       double xi2,    double xi3,    double xl2,     double xl3,    double xl4,
-       double zmol,   double zmos,   double inclo,
-       char init,
-       double& ep,    double& inclp, double& nodep,  double& argpp, double& mp,
-       char opsmode
-     );
-
-static void dscom
-     (
-       double epoch,  double ep,     double argpp,   double tc,     double inclp,
-       double nodep,  double np,
-       double& snodm, double& cnodm, double& sinim,  double& cosim, double& sinomm,
-       double& cosomm,double& day,   double& e3,     double& ee2,   double& em,
-       double& emsq,  double& gam,   double& peo,    double& pgho,  double& pho,
-       double& pinco, double& plo,   double& rtemsq, double& se2,   double& se3,
-       double& sgh2,  double& sgh3,  double& sgh4,   double& sh2,   double& sh3,
-       double& si2,   double& si3,   double& sl2,    double& sl3,   double& sl4,
-       double& s1,    double& s2,    double& s3,     double& s4,    double& s5,
-       double& s6,    double& s7,    double& ss1,    double& ss2,   double& ss3,
-       double& ss4,   double& ss5,   double& ss6,    double& ss7,   double& sz1,
-       double& sz2,   double& sz3,   double& sz11,   double& sz12,  double& sz13,
-       double& sz21,  double& sz22,  double& sz23,   double& sz31,  double& sz32,
-       double& sz33,  double& xgh2,  double& xgh3,   double& xgh4,  double& xh2,
-       double& xh3,   double& xi2,   double& xi3,    double& xl2,   double& xl3,
-       double& xl4,   double& nm,    double& z1,     double& z2,    double& z3,
-       double& z11,   double& z12,   double& z13,    double& z21,   double& z22,
-       double& z23,   double& z31,   double& z32,    double& z33,   double& zmol,
-       double& zmos
-     );
-
-static void dsinit
-     (
-       gravconsttype whichconst,
-       double cosim,  double emsq,   double argpo,   double s1,     double s2,
-       double s3,     double s4,     double s5,      double sinim,  double ss1,
-       double ss2,    double ss3,    double ss4,     double ss5,    double sz1,
-       double sz3,    double sz11,   double sz13,    double sz21,   double sz23,
-       double sz31,   double sz33,   double t,       double tc,     double gsto,
-       double mo,     double mdot,   double no,      double nodeo,  double nodedot,
-       double xpidot, double z1,     double z3,      double z11,    double z13,
-       double z21,    double z23,    double z31,     double z33,    double ecco,
-       double eccsq,  double& em,    double& argpm,  double& inclm, double& mm,
-       double& nm,    double& nodem,
-       int& irez,
-       double& atime, double& d2201, double& d2211,  double& d3210, double& d3222,
-       double& d4410, double& d4422, double& d5220,  double& d5232, double& d5421,
-       double& d5433, double& dedt,  double& didt,   double& dmdt,  double& dndt,
-       double& dnodt, double& domdt, double& del1,   double& del2,  double& del3,
-       double& xfact, double& xlamo, double& xli,    double& xni
-     );
-
-static void dspace
-     (
-       int irez,
-       double d2201,  double d2211,  double d3210,   double d3222,  double d4410,
-       double d4422,  double d5220,  double d5232,   double d5421,  double d5433,
-       double dedt,   double del1,   double del2,    double del3,   double didt,
-       double dmdt,   double dnodt,  double domdt,   double argpo,  double argpdot,
-       double t,      double tc,     double gsto,    double xfact,  double xlamo,
-       double no,
-       double& atime, double& em,    double& argpm,  double& inclm, double& xli,
-       double& mm,    double& xni,   double& nodem,  double& dndt,  double& nm
-     );
-
-static void initl
-     (
-       int satn,      gravconsttype whichconst,
-       double ecco,   double epoch,  double inclo,   double& no,
-       char& method,
-       double& ainv,  double& ao,    double& con41,  double& con42, double& cosio,
-       double& cosio2,double& eccsq, double& omeosq, double& posq,
-       double& rp,    double& rteosq,double& sinio , double& gsto, char opsmode
-     );
-
+"""
 /* -----------------------------------------------------------------------------
 *
 *                           procedure dpper
@@ -2118,7 +2052,4 @@ void getgravconst
 
      }   // end getgravconst
 
-
-
-
-
+"""
