2315e9622bb1143f9d0f6f1ec504a31f4ec83186
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-25 21:24:30 -0400

    Refactored the test logic using a generator

--- sgp4/tests.py
+++ sgp4/tests.py
@@ -5,50 +5,60 @@ from unittest import TestCase
 from sgp4.io import twoline2rv
 from sgp4.propagation import sgp4
 
+thisdir = os.path.dirname(__file__)
+
+
 class SatRec(object):
     pass
 
 
 class Tests(TestCase):
 
-    def test_tle(self):
-        dirpath = os.path.dirname(__file__)
-        tlepath = os.path.join(dirpath, 'SGP4-VER.TLE')
-        with open(tlepath) as tlefile:
-            lines = tlefile.readlines()
-
+    def test_tle_verify(self):
         whichconst = 'wgs72'
+        actual = generate_test_output(whichconst)
 
-        for i in range(len(lines)):
+        tcppath = os.path.join(thisdir, 'tcppver.out')
+        with open(tcppath) as tcpfile:
+            for i, expected_line in enumerate(tcpfile):
+                try:
+                    actual_line = next(actual)
+                except StopIteration:
+                    print 'WARNING: our output ended early, on line %d' % (i+1)
+                    break
+                if actual_line != expected_line:
+                    raise ValueError(
+                        'Line %d of output does not match:\n'
+                        '\n'
+                        'Expect: %s'
+                        'Actual: %s'
+                        % (i + 1, expected_line, actual_line))
 
-            if not lines[i].startswith('1'):
-                continue
 
-            line1 = lines[i]
-            line2 = lines[i + 1]
+def generate_test_output(whichconst):
+    """Generate lines like those in the test file tcppver.out."""
 
-            satrec = SatRec()
+    tlepath = os.path.join(thisdir, 'SGP4-VER.TLE')
+    with open(tlepath) as tlefile:
+        tlelines = iter(tlefile.readlines())
 
-            twoline2rv(line1, line2, 'c', None, 'i', whichconst, satrec)
+    for line1 in tlelines:
 
-            # Call the propagator to get the initial state vector value.
-            ro = [0.0, 0.0, 0.0]
-            vo = [0.0, 0.0, 0.0]
-            sgp4(whichconst, satrec,  0.0, ro,  vo);
+        if not line1.startswith('1'):
+            continue
 
-            tcppath = os.path.join(dirpath, 'tcppver.out')
-            with open(tcppath) as tcpfile:
-                tcplines = tcpfile.readlines()
+        line2 = next(tlelines)
 
-            line0 = '%ld xx\n' % (satrec.satnum,)
-            line1 = ' %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f\n' % (
-                satrec.t, ro[0], ro[1], ro[2], vo[0], vo[1], vo[2])
-            print tcplines[0] == line0
-            print tcplines[1] == line1
-            break
-            sgp4(whichconst, satrec,  360.0, ro,  vo);
-            print satrec.t, ro[0], ro[1], ro[2], vo[0], vo[1], vo[2]
-            print ' '.join(repr(float(field)) for field
-                           in tcplines[2].split()[:7])
+        satrec = SatRec()
+        twoline2rv(line1, line2, 'c', None, 'i', whichconst, satrec)
 
-            break
+        # Call the propagator to get the initial state vector value.
+        ro = [0.0, 0.0, 0.0]
+        vo = [0.0, 0.0, 0.0]
+        sgp4(whichconst, satrec,  0.0, ro,  vo)
+
+        yield '%ld xx\n' % (satrec.satnum,)
+        yield ' %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f\n' % (
+            satrec.t, ro[0], ro[1], ro[2], vo[0], vo[1], vo[2])
+        return
+        sgp4(whichconst, satrec,  360.0, ro,  vo);
