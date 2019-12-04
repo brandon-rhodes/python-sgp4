d38342a2f89029f387895b6d2beffa84c850c2d7
Brandon Rhodes <brandon@rhodesmill.org>
2019-12-03 19:38:41 -0500

    Turn tmp.py into a simple benchmark

--- tmp.py
+++ tmp.py
@@ -1,35 +1,72 @@
+# ----------------------------------------------------
+# Based on the README at https://github.com/bwinkel/cysgp4
+
+import numpy as np
+from cysgp4 import PyTle, PyObserver, propagate_many
+
+with open('science.txt') as f:
+    text = f.read()
+
+all_lines = text.splitlines()
+
+# Need to convert them to a list of tuples (each tuple consisting
+# of the three TLE strings)
+tle_list = list(zip(*tuple(
+    all_lines[idx::3] for idx in range(3)
+    )))
+# Create an array of PyTle and PyObserver objects, and MJDs
+tles = np.array([
+    PyTle(*tle) for tle in tle_list
+    ])[np.newaxis, np.newaxis, :20]  # use first 20 TLEs
+mjds = np.linspace(
+    58805.5, 58806.5, 1000  # 1000 time steps
+    )[:, np.newaxis, np.newaxis]
+
+t0 = __import__('time').time()
+result = propagate_many(
+    mjds, tles, None,
+    do_eci_pos=True, do_eci_vel=True, do_geo=False, do_topo=False
+    )
+print('TIME:', __import__('time').time() - t0)
+
+print(result.keys())
+print(result['eci_pos'].shape)
+print(result['eci_pos'][0])
+
+# -------------------------------------
+
 import sys
-del sys.path[0]
-import sgp4
-import sgp4.vallado_cpp
-print(dir(sgp4.vallado_cpp))
+del sys.path[0]  # prevent importing from "sgp4/" in this directory
 
-# s = sgp4.vallado_cpp.Satrec()
-# print(s)
-# print("len", len(s))
+from sgp4.vallado_cpp import Satrec, SatrecArray
 
-line1 = '1 25544U 98067A   14020.93268519  .00009878  00000-0  18200-3 0  5082'
-line2 = '2 25544  51.6498 109.4756 0003572  55.9686 274.8005 15.49815350868473'
+sats = []
+lines = iter(text.splitlines())
+for name in lines:
+    line1 = next(lines)
+    line2 = next(lines)
+    sat = Satrec.twoline2rv(line1, line2)
+    # print(sat)
+    # print(sat.satnum)
+    # print(sat.method)
+    sats.append(sat)
 
-s = sgp4.vallado_cpp.Satrec.twoline2rv(line1, line2)
+a = SatrecArray(sats[:20])
+print("Number of satellites:", len(a))
 
-print(s)
-print(s.satnum)
-print(s.method)
+mjd = np.linspace(58805.5, 58806.5, 1000)  # 1000 time steps
 
-print(s.sgp4(0))
-print(s.sgp4(1))
+# t = array([0.0, 1, 2, 3, 49999999])
+t = mjd
+r = np.ndarray((len(a), len(t), 3))
+v = np.ndarray((len(a), len(t), 3))
+e = np.ndarray((len(a), len(t)), 'uint8')
 
-a = sgp4.vallado_cpp.SatrecArray([s, s])
-print("len", len(a))
-
-from numpy import array, ndarray
-t = array([0.0, 1, 2, 3, 49999999])
-r = ndarray((len(a), len(t), 3))
-v = ndarray((len(a), len(t), 3))
-e = ndarray((len(a), len(t)), 'uint8')
-print(v)
+t0 = __import__('time').time()
 print(a.sgp4(t, r, v, e))
-print(r)
-print(v)
-print(e)
+print(__import__('time').time() - t0)
+
+print(r.shape)
+print(r[0])
+# print(v)
+# print(e)
