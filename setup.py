1d73080882e21411f0dccd0ef336d23c34bc2ad8
Brandon Rhodes <brandon@rhodesmill.org>
2020-01-30 12:07:56 -0500

    Declare version 2.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if sys.version_info[0] == 3:
     ))
 
 setup(name = 'sgp4',
-      version = '1.4',
+      version = '2.0',
       description = description,
       long_description = long_description,
       license = 'MIT',
