7a32754fa4b5327d20f548c276be86c910eb73c0
Brandon Rhodes <brandon@rhodesmill.org>
2020-03-21 16:06:35 -0400

    Move constants import where errors can be caught
    The point of the try..except is to determine if the C++ module is able
    to be imported at all, not to detect whether the expected symbols are
    available inside; so let’s move the second import to catch ourselves in
    case we ever incorrectly name the constants.

--- sgp4/api.py
+++ sgp4/api.py
@@ -15,12 +15,10 @@ SGP4_ERRORS = {
 
 try:
     from .wrapper import Satrec, SatrecArray
-    from .vallado_cpp import WGS72OLD, WGS72, WGS84
-
     accelerated = True
-
 except ImportError:
     from .model import Satrec, SatrecArray
     from .model import WGS72OLD, WGS72, WGS84
-
     accelerated = False
+else:
+    from .vallado_cpp import WGS72OLD, WGS72, WGS84
