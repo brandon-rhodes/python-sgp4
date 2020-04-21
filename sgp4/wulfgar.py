cb07b0069d0efe0fecfbb2e4b1701bcd34fab037
Brandon Rhodes <brandon@rhodesmill.org>
2020-04-21 17:33:10 -0400

    Fix test detection in non-top-level modules

--- sgp4/wulfgar.py
+++ sgp4/wulfgar.py
@@ -1,24 +1,20 @@
 """Support test functions, in a module small enough to carry inline."""
 
+from importlib import import_module
 from unittest import TestCase
 __unittest = 1  # Tell unittest not to include run() in test tracebacks.
 
 def add_test_functions(loader, tests, module_name):
     """Run our main documentation as a test and test functions in this file."""
 
-    module = __import__(module_name)
-
     def wrap_test_function(test):
         def run(self):
             return test()
         return run
 
-    test_functions = [getattr(module, name) for name in dir(module)
-                      if name.startswith('test_')]
-
-    class TestFunctions(TestCase): pass
-
-    for f in test_functions:
-        setattr(TestFunctions, f.__name__, wrap_test_function(f))
-
+    module = import_module(module_name)
+    test_methods = dict((name, wrap_test_function(getattr(module, name)))
+                        for name in dir(module) if name.startswith('test_'))
+    TestFunctions = type('TestFunctions', (TestCase,), test_methods)
+    TestFunctions.__module__ = module_name
     tests.addTest(loader.loadTestsFromTestCase(TestFunctions))
