9bae19b89d015a5ca1dd96b813043cd8156d2e4a
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2025-10-15 18:09:09 -0600

    Python 3.14 support

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Scientific/Engineering :: Astronomy',
     ],
     packages = ['sgp4'],
